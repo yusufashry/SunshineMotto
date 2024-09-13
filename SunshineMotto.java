3d07741c6d2069347560ff8708c2bb624892f962
yusufashry <yusuf.ashry@student.lssc.edu>
2024-09-13 19:33:22 -0400

    Update SunshineMotto.java

--- SunshineMotto.java
+++ SunshineMotto.java
@@ -1,5 +1,5 @@
 public class SunshineMotto { 
   public static void main(String[] args) {
-      System.out.println("Sunshine Seashore makes it fun in the sun.") 
+      System.out.println("Sunshine Seashore makes it fun in the sun.");
   }
 }
