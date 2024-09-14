0eb531db608cd8cf638160e74ecd6592933620d5
yusufashry <yusuf.ashry@student.lssc.edu>
2024-09-13 20:07:09 -0400

    Update SunshineMotto2.java

--- SunshineMotto2.java
+++ SunshineMotto2.java
@@ -1,9 +1,9 @@
 public class SunshineMotto2 { 
   public static void main(String[] args) {
-    String motto = "Yummy makes the food that makes it a party";
+    String motto = "Yummy makes the food that makes it a party.";
     int borderlength = motto.length() +4;
-          printBorder(borderLength);
-      System.out.println("S" + motto + "S");
-          printBorder(borderLength);
+    printBorder(borderLength);
+      System.out.println("S " + motto + " S");
+    printBorder(borderLength);
   }
 }
